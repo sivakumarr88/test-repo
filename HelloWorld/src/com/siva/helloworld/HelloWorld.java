33eb80cee0cd13ff3ff73f1299066d425d197217
sivakumarr88 <41332747+sivakumarr88@users.noreply.github.com>
2022-08-16 01:17:30 +0530

    edited second msg value

--- HelloWorld/src/com/siva/helloworld/HelloWorld.java
+++ HelloWorld/src/com/siva/helloworld/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		String str = "Hello World..., what's up";
 		System.out.println(str);
-		String str1 = "second message";
+		String str1 = "second msg";
 	}
 
 }
