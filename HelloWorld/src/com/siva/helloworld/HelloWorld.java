41b7ccaea2ffb71b58caba6f1c481191f2c3f1c9
sivakumarr88 <sivakumar.ramalingam88@gmail.com>
2022-08-16 01:33:56 +0530

    added method1

--- HelloWorld/src/com/siva/helloworld/HelloWorld.java
+++ HelloWorld/src/com/siva/helloworld/HelloWorld.java
@@ -2,6 +2,10 @@ package com.siva.helloworld;
 
 public class HelloWorld {
 	
+	public static void method1() {
+		System.out.println("method1");
+	}
+	
 	public static void main(String[] args) {
 		String str = "Hello World..., what's up";
 		System.out.println(str);
